b739f25fa197026b1d1a97348ed3c1afdaca42de
Sasha Klein <sashafklein@gmail.com>
2022-02-13 16:44:26 -0800

    Debugging log

--- src/shared/global.tsx
+++ src/shared/global.tsx
@@ -3,7 +3,7 @@ import { ThemeProvider, StyledEngineProvider } from "@mui/material/styles";
 import { theme } from "./theme";
 
 import variables from "!style-loader!css-loader!sass-loader!./scss/theme.module.scss";
-
+console.log(variables);
 export const GlobalStyleWrap = ({ children }: { children: React.Component }): ReactElement => (
   <StyledEngineProvider injectFirst>
     <ThemeProvider theme={theme(variables)}>{children}</ThemeProvider>
